b3f99d699ae3c6393eb13445f449face21fd1c8c
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-27 14:52:56 +0530

    Day 1

---
+++ Day01.java
@@ -0,0 +1,22 @@
+// Day 1 - Second Largest Element in an Array //
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int a = arr.length;
+        if (a<2){
+            return -1;
+        }
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i=0; i<a; i++){
+            if(arr[i]>first){
+                second = first;
+                first = arr[i];
+            }
+            else if(arr[i]>second && arr[i]!=first){
+                second=arr[i];
+            }
+        }
+        return(second==Integer.MIN_VALUE) ? -1 :second;
+    }
+}
