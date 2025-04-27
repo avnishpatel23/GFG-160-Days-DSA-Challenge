e13e199d9b7c2007e7b3c03913d9c5edbc69baba
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-27 14:53:40 +0530

    Day 3

---
+++ Day03.java
@@ -0,0 +1,15 @@
+// Day 3 - Reverse an Array
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        int start = 0;
+        int end = arr.length-1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
