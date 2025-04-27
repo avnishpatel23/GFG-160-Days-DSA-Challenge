56853413f50f8c06b4b14a9d9b6e8eac9f2fbf07
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-28 00:56:37 +0530

    Day 10

---
+++ Day10.java
@@ -0,0 +1,17 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        int n = arr.length;
+        int ans = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            ans = Math.max(ans, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return ans;
+    }
+}
