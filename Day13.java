b1e063e850164029e940b6bccb85230af0a4bb21
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:20:07 +0530

    Day 13

---
+++ Day13.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    // Function to find the smallest positive missing number
+    public int missingNumber(int[] arr) {
+        Arrays.sort(arr);
+        int missing = 1;
+
+        for (int num : arr) {
+            if (num == missing) {
+                missing++;
+            }
+        }
+
+        return missing;
+    }
+}
