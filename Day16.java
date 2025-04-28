81242150d690bea2d1d8c8e7cde8a1c8ac068d3e
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:21:21 +0530

    Day 16

---
+++ Day16.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] res = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            res[s1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            res[s2.charAt(i) - 'a']--;
+        }
+
+        for (int count : res) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
