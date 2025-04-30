7a270ab1e788e9640c974ffe2032fb513cef1a60
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-30 08:19:10 +0530

    Day 21

---
+++ Day21.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    int temp0 = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    int temp2 = arr[high];
+                    arr[high] = arr[mid];
+                    arr[mid] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+}
