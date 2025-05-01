67330489fb9857bcf5323029d945b60134f20ed8
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-01 09:16:53 +0530

    Day 29

---
+++ Day29.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int i, min = arr[0];
+        for(i=0; i<arr.length; i++)
+        {
+            if(arr[i]<min) min = arr[i];
+        }
+        return min;
+    }
+}
