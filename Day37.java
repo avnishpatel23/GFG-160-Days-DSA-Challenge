6c4bebb9be8ffbac817ab4205b913f38bcfe1427
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-07 00:22:43 +0530

    Day 37 - Rotate by 90 degree

---
+++ Day37.java
@@ -0,0 +1,26 @@
+// Rotate by 90 degree
+
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int mat[][]) {
+        int n = mat.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int j=0; j<n; j++){
+            int top = 0;
+            int bottom = n-1;
+            while(top<bottom){
+                int temp = mat[top][j];
+                mat[top][j] = mat[bottom][j];
+                mat[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+}
