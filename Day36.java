948ec89e45e5a38dedb867832446de23fec6d53b
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-06 18:54:33 +0530

    Day 36 - Spirally traversing a matrix

---
+++ Day36.java
@@ -0,0 +1,41 @@
+// Spirally traversing a matrix
+
+
+class Solution {
+    public ArrayList<Integer> spirallyTraverse(int mat[][]) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int left = 0;
+        int right = m - 1;
+        int top = 0;
+        int bottom = n - 1;
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                ans.add(mat[top][i]);
+            }
+            top++;
+            
+            for (int i = top; i <= bottom; i++) {
+                ans.add(mat[i][right]);
+            }
+            right--;
+            
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
