d01a67879b7510751a84edf014b558b7b90f81ac
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-15 17:10:28 +0530

    Day45 IntersectionOfTwoArraysWithDuplicateElements

---
+++ Day45.java
@@ -0,0 +1,18 @@
+// Intersection of Two arrays with Duplicate Elements
+
+class Solution {
+    public ArrayList<Integer> intersectionWithDuplicates(int[] a, int[] b) {
+        HashSet<Integer> A = new HashSet<>();
+        for(int num : a){
+            A.add(num);
+        }
+        HashSet<Integer> result = new HashSet<>();
+        for(int num : b){
+            if(A.contains(num)){
+                result.add(num);
+                A.remove(num);
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
