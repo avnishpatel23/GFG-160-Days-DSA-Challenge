76b4754b46c4acef257285115815781d3811568b
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-01 16:29:49 +0530

    Day 62 - Longest Subarray with Sum K

---
+++ Day62.java
@@ -0,0 +1,22 @@
+// Longest Subarray with Sum K
+
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int max = 0;
+        for(int i=-0; i<arr.length; i++){
+            sum += arr[i];
+            if(sum==k){
+                max = i+1;
+            }
+            if(map.containsKey(sum-k)){
+                max=Math.max(max, i-map.get(sum-k));
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+        }
+        return max;
+    }
+}
