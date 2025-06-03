01e854024a3a8cceeba2bf9ffe9e0b68ef52c7ba
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-03 12:20:30 +0530

    Day 64 - Product array puzzle

---
+++ Day64.java
@@ -0,0 +1,19 @@
+// Product array puzzle
+
+
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        int n = arr.length;
+        int [] res = new int [n];
+        res [0] = 1;
+        for(int i=1; i<n; i++){
+            res[i] = res[i-1]*arr[i-1];
+        }
+        int suffixProduct = 1;
+        for(int i=n-1; i>=0; i--){
+            res[i]*= suffixProduct;
+            suffixProduct *=arr[i];
+        }
+        return res;
+    }
+}
