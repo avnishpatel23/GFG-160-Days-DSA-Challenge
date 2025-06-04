920f2c748b9681e4cee882886996d1748399575c
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-04 07:19:18 +0530

    Day 65 - Reverse a Linked List

---
+++ Day65.java
@@ -0,0 +1,14 @@
+// Reverse a Linked List
+
+class Solution {
+    Node reverseList(Node head) {
+        Node prev = null;
+        while(head != null){
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
