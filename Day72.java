37a39ef207cd7fc1407107dfd948ec5fa8099b26
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-12 06:59:07 +0530

    Day 72 - First Node of the Cycle

---
+++ Day72.java
@@ -0,0 +1,22 @@
+// Day 72 - First Node of the Cycle
+
+
+class Solution {
+    public static Node findFirstNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return new Node(-1);
+    }
+}
