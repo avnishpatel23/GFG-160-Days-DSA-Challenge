edd6cb273555432c918b54542379b701bdf61b05
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-21 10:08:57 +0530

    Day 81 - Height of Binary Tree

---
+++ Day81.java
@@ -0,0 +1,14 @@
+// Height of Binary Tree
+
+
+class Solution {
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        if(node == null)
+        return -1;
+        int lh = height(node.left);
+        int rh = height(node.right);
+        
+        return 1 + Math.max(lh, rh);
+    }
+}
