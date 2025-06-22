52b4cd2b356fb0b9f79aa87afe07b70c01faae3d
Avnish Patel <er.avnishpatel023@gmail.com>
2025-06-22 19:14:36 +0530

    Day 83 - Mirror Tree

---
+++ Day83.java
@@ -0,0 +1,35 @@
+// Mirror Tree
+
+
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        if(node == null){
+            return;
+        }
+        mirror(node.left);
+        mirror(node.right);
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+    static void levelOrder(Node root){
+        if(root == null){
+            System.out.print("N");
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node curr = queue.poll();
+            if(curr == null){
+                System.out.print("N");
+                continue;
+            }
+            System.out.print(curr.data + " ");
+            queue.add(curr.left);
+            queue.add(curr.right);
+        }
+    }
+}
